d3a4733c2d06d91e726ff78fb0cb485af5542b49
Emad0007 <BASEMRF007@GMAIL.COM>
2022-06-20 16:22:17 +0100

    finly add  number

---
+++ numbers.js
@@ -0,0 +1,49 @@
+'use strict'
+/*const integer = 102
+const float = 13.9
+
+console.log (integer , float)*/
+
+
+/*const basic = 34
+const stringified = basic.toString()
+console.log(stringified)
+cosole.log(typeof stringified */
+
+
+
+/*const num = 1.5
+const rounded = Math.round(num)
+console.log(rounded)*/
+
+
+/*const test = 12
+const bis = 5
+const total= test + bis
+const total2= test - bis
+const total3= test / bis
+const total4= test * bis
+const total5= test % bis
+
+console.log(total ,total2 ,total3, total4 , )*/
+
+/*console.log( 25 % 5) 
+console.log(`1 et 5 divise sur 25`)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
