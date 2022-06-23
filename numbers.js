852a67064ce3e34f165b0a7cc200885f6d0e01d5
Emad0007 <BASEMRF007@GMAIL.COM>
2022-06-23 06:32:15 +0100

    add question to solvs

--- numbers.js
+++ numbers.js
@@ -1,34 +1,63 @@
-'use strict'
-/*const integer = 102
-const float = 13.9
+// 'use strict'
 
-console.log (integer , float)*/
+// ## 01 - Number
 
+// - Créez une const `integer` et donnez lui la valeur `102`
+// - Créer une const `float` et donnez lui la valeur `13.9` (🚨on utilise le point pour les décimaux quand on code !)
+// - Affichez dans la console
+// /*const integer = 102
+// const float = 13.9
 
-/*const basic = 34
-const stringified = basic.toString()
-console.log(stringified)
-cosole.log(typeof stringified */
-
-
-
-/*const num = 1.5
-const rounded = Math.round(num)
-console.log(rounded)*/
+// console.log (integer , float)*/
 
 
-/*const test = 12
-const bis = 5
-const total= test + bis
-const total2= test - bis
-const total3= test / bis
-const total4= test * bis
-const total5= test % bis
+// /*const basic = 34
+// const stringified = basic.toString()
+// console.log(stringified)
+// cosole.log(typeof stringified */
 
-console.log(total ,total2 ,total3, total4 , total5*/
 
-/*console.log( 25 % 5) 
-console.log(`1 et 5 divise sur 25`)*/
+// ## 02 - Convert
+
+// - Créez une const `basic` de valeur `34`
+// - Créez une const `stringified` qui contient la version string de `basic`
+// - Affichez `stringified` dans la console
+
+// ## 03 - Round
+
+// - Créez une const `num` de valeur `1.5`
+// - Créez une const `rounded` qui contient l'arrondi de `num`
+// - Affichez `rounded` dans la console
+
+// /*const num = 1.5
+// const rounded = Math.round(num)
+// console.log(rounded)*/
+
+// ## 04 - Arithmétique
+
+// - Créer une const `test` de valeur `12` et une const `bis` de valeur `5`
+// - Utilisez tous les opérateurs arithmétiques sur ces variables et affichez les résultats (+, -, /, x, %)
+// /*
+// const test = 12
+// const bis = 5
+// const total= test + bis
+// const total2= test - bis
+// const total3= test / bis
+// const total4= test * bis
+// const total5= test % bis
+
+// console.log(total ,total2 ,total3, total4 , total5*/
+
+
+// ## 05 - Reste division euclidienne
+
+// - Entre les chiffres 1, 2, 3, 4 et 5, trouvez ceux divisent complètement le chiffre `999`, et affichez les
+// - Entre les chiffres 1, 2, 3, 4 et 5, trouvez ceux divisent complètement le chiffre `25`, et affichez les
+// - Entre les chiffres 1, 2, 3, 4 et 5, trouvez ceux divisent complètement le chiffre `190280`, et affichez les
+// - Entre les chiffres 1, 2, 3, 4 et 5, trouvez ceux divisent complètement le chiffre `47`, et affichez les
+// - Entre les chiffres 1, 2, 3, 4 et 5, trouvez ceux divisent complètement le chiffre `691`, et affichez les
+// /*console.log( 25 % 5) 
+// console.log(`1 et 5 divise sur 25`)*/
 
 
 
